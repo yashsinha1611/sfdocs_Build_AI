b00f0c7395e338210a57bd8bab466a8e8eac830d
Anbukumar <anbukumar.c@maplelabs.com>
2021-09-28 16:52:59 +0530

    Table content when is not visible in article section, always showing the table content right side .

--- Docusaurus/sf-documentation/src/pages/index.jquery.js
+++ Docusaurus/sf-documentation/src/pages/index.jquery.js
@@ -16,22 +16,26 @@ class IndexJquery extends React.Component {
     $('article header:first-child').hide()
    } 
   if($(window).width() > 1000){ 
-      $('main .col--3 ul.table-of-contents').hide()
+     $('main .col--3 ul.table-of-contents').hide()
       window.addEventListener('scroll', (event) => {
-        const number = $(window).scrollTop(); 
-        if (number > $('article ul.table-of-contents').height()) {
-            $('main .col--3 ul.table-of-contents').fadeIn();
-        } else{
-          $('main .col--3 ul.table-of-contents').fadeOut();
-        }
+        if($('article ul').hasClass('table-of-contents')){ 
+          const number = $(window).scrollTop(); 
+          if (number > $('article ul.table-of-contents').height()) {
+              $('main .col--3 ul.table-of-contents').fadeIn();
+          } else{
+            $('main .col--3 ul.table-of-contents').fadeOut();
+          }
+        } 
       });
       window.addEventListener('resize',(event) => {
+        if($('article ul').hasClass('table-of-contents')){ 
         const number = $(window).scrollTop(); 
         if (number > $('article ul.table-of-contents').height()) {
             $('main .col--3 ul.table-of-contents').fadeIn();
         } else{
           $('main .col--3 ul.table-of-contents').fadeOut();
         }
+      } 
       });
     }
     } 
