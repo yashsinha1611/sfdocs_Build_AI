fe3f10d867a1d5b23844df00a1f78408a1d2d735
swastiksingh <swastiksingh@commvault.com>
2021-09-29 14:27:44 +0530

    Implemented search and category filters for App Integrations

--- Docusaurus/sf-documentation/docs/Integrations/constants/index.js
+++ Docusaurus/sf-documentation/docs/Integrations/constants/index.js
@@ -1,229 +1,262 @@
-import React from "react";
+import React, {useState} from "react";
+import $ from 'jquery';
 
-export const AGENTS =  [
+const AGENTS =  [
 	{
 		'src':'/img/mysql-logo.svg',
 		'label':'MySQL',
-		'overText': 'MySQL Infrastructure platform is built for enterprises'
+		'overText': 'MySQL Infrastructure platform is built for enterprises',
+		'category': 'Databases'
 	},
 	{
 		'src':'/img/postgres-logo.svg',
 		'label':'Postgres',
-		'overText': 'Postgres Infrastructure platform is built for enterprises'
+		'overText': 'Postgres Infrastructure platform is built for enterprises',
+		'category': 'Databases'
 	},
 	{
 		'src':'/img/java.svg',
 		'label':'Java',
-		'overText': 'Java Infrastructure platform is built for enterprises'
+		'overText': 'Java Infrastructure platform is built for enterprises',
+		'category': 'App Tier'
 	},
 	{
 		'src':'/img/oracle_1.svg',
 		'label':'Oracle',
-		'overText': 'Oracle Cloud Infrastructure platform is built for enterprises'
+		'overText': 'Oracle Cloud Infrastructure platform is built for enterprises',
+		'category': 'Databases'
 	},
 	{
 		'src':'/img/mongodb.svg',
 		'label':'MongoDB',
-		'overText': 'MongoDB Infrastructure platform is built for enterprises'
+		'overText': 'MongoDB Infrastructure platform is built for enterprises',
+		'category': 'Databases'
 	},
 	{
 		'src':'/img/oracle.svg',
 		'label':'Microsoft SQL Server',
-		'overText': 'Microsoft Infrastructure platform is built for enterprises'
+		'overText': 'Microsoft Infrastructure platform is built for enterprises',
+		'category': 'Databases'
 	},
 	{
 		'src':'/img/cassandra.svg',
 		'label':'Cassandra',
-		'overText': 'Cassandra Infrastructure platform is built for enterprises'
+		'overText': 'Cassandra Infrastructure platform is built for enterprises',
+		'category': 'Databases'
 	}, 
 	{
 		'src':'/img/kafka.svg',
 		'label':'Kafka',
-		'overText': 'Kafka Infrastructure platform is built for enterprises'
+		'overText': 'Kafka Infrastructure platform is built for enterprises',
+		'category': 'App Tier'
 	},
 	{
 		'src':'/img/ldap.svg',
 		'label':'Ldap',
-		'overText': 'Ldap Infrastructure platform is built for enterprises'
+		'overText': 'Ldap Infrastructure platform is built for enterprises',
+		'category': 'Authentication'
 	},
 	{
 		'src':'/img/kubernetes-clusters.svg',
 		'label':'Kubernetes Clusters',
-		'overText': 'Kubernetes clusters Infrastructure platform is built for enterprises'
+		'overText': 'Kubernetes clusters Infrastructure platform is built for enterprises',
+		'category': 'Cloud'
 	},
 	{
 		'src':'/img/mysql.svg',
 		'label':'MYSQL',
-		'overText': 'MYSQL Infrastructure platform is built for enterprises'
+		'overText': 'MYSQL Infrastructure platform is built for enterprises',
+		'category': 'Databases'
 	},
 	{
 		'src':'/img/okta.svg',
 		'label':'Okta',
-		'overText': 'Okta Infrastructure platform is built for enterprises'
+		'overText': 'Okta Infrastructure platform is built for enterprises',
+		'category': 'Authentication'
 	},
 	{
 		'src':'/img/nginx.svg',
 		'label':'Nginx',
-		'overText': 'Nginx Infrastructure platform is built for enterprises'
-	},
-	{
-		'src':'/img/nginx-1.svg',
-		'label':'Nginx',
-		'overText': 'Nginx Infrastructure platform is built for enterprises'
+		'overText': 'Nginx Infrastructure platform is built for enterprises',
+		'category': 'Web Tier'
 	},
 	{
 		'src':'/img/node-js.svg',
 		'label':'NodeJS',
-		'overText': 'NodeJS Infrastructure platform is built for enterprises'
+		'overText': 'NodeJS Infrastructure platform is built for enterprises',
+		'category': 'App Tier'
 	},
 	{
 		'src':'/img/onelogin.svg',
 		'label':'OneLogin',
-		'overText': 'OneLogin Infrastructure platform is built for enterprises'
+		'overText': 'OneLogin Infrastructure platform is built for enterprises',
+		'category': 'Authentication'
 	},
 	{
 		'src':'/img/oozie.svg',
 		'label':'Oozie',
-		'overText': 'Oozie Infrastructure platform is built for enterprises'
+		'overText': 'Oozie Infrastructure platform is built for enterprises',
+		'category': 'App Tier'
 	},
 	{
 		'src':'/img/opsgenie.svg',
 		'label':'Opsgenie',
-		'overText': 'Opsgenie Infrastructure platform is built for enterprises'
+		'overText': 'Opsgenie Infrastructure platform is built for enterprises',
+		'category': 'Alerts & Notifications'
 	},
 	{
 		'src':'/img/pagerduty.svg',
 		'label':'Pagerduty',
-		'overText': 'Pagerduty Infrastructure platform is built for enterprises'
+		'overText': 'Pagerduty Infrastructure platform is built for enterprises',
+		'category': 'Alerts & Notifications'
 	},
 	{
 		'src':'/img/postgres-logo.svg',
 		'label':'Postgres',
-		'overText': 'Postgres Infrastructure platform is built for enterprises'
+		'overText': 'Postgres Infrastructure platform is built for enterprises',
+		'category': 'Databases'
 	},
 	{
 		'src':'/img/prometheus.svg',
 		'label':'Prometheus',
-		'overText': 'Prometheus Infrastructure platform is built for enterprises'
+		'overText': 'Prometheus Infrastructure platform is built for enterprises',
+		'category': 'Cloud'
 	},
 	{
 		'src':'/img/public-cloud-elbs.svg',
 		'label':'Public cloud elbs',
-		'overText': 'Public cloud elbs Infrastructure platform is built for enterprises'
+		'overText': 'Public cloud elbs Infrastructure platform is built for enterprises',
+		'category': 'Cloud'
 	},
 	{
 		'src':'/img/python.svg',
 		'label':'Python',
-		'overText': 'Python Infrastructure platform is built for enterprises'
+		'overText': 'Python Infrastructure platform is built for enterprises',
+		'category': 'App Tier'
 	},
 	{
 		'src':'/img/redis.svg',
 		'label':'Redis',
-		'overText': 'Redis Infrastructure platform is built for enterprises'
+		'overText': 'Redis Infrastructure platform is built for enterprises',
+		'category': 'App Tier'
 	},
 	{
 		'src':'/img/spark.svg',
 		'label':'Spark',
-		'overText': 'Spark Infrastructure platform is built for enterprises'
+		'overText': 'Spark Infrastructure platform is built for enterprises',
+		'category': 'Web Tier'
 	},
 	{
 		'src':'/img/stack.svg',
 		'label':'Stack',
-		'overText': 'Stack Infrastructure platform is built for enterprises'
+		'overText': 'Stack Infrastructure platform is built for enterprises',
+		'category': 'Alerts & Notifications'
 	},
 	{
 		'src':'/img/teams.svg',
 		'label':'Teams',
-		'overText': 'Teams Infrastructure platform is built for enterprises'
+		'overText': 'Teams Infrastructure platform is built for enterprises',
+		'category': 'Alerts & Notifications'
 	},
 	{
 		'src':'/img/saml.svg',
 		'label':'Saml',
-		'overText': 'Saml Infrastructure platform is built for enterprises'
+		'overText': 'Saml Infrastructure platform is built for enterprises',
+		'category': 'Authentication'
 	},
 	{
 		'src':'/img/v-center.svg',
 		'label':'vCenter',
-		'overText': 'vCenter Infrastructure platform is built for enterprises'
+		'overText': 'vCenter Infrastructure platform is built for enterprises',
+		'category': 'Cloud'
 	},
 	{
 		'src':'/img/windows-vms.svg',
 		'label':'Windows VMs',
-		'overText': 'Windows VMs Infrastructure platform is built for enterprises'
+		'overText': 'Windows VMs Infrastructure platform is built for enterprises',
+		'category': 'Cloud'
 	},
 	{
 		'src':'/img/zenduty.svg',
 		'label':'Zenduty',
-		'overText': 'Zenduty Infrastructure platform is built for enterprises'
+		'overText': 'Zenduty Infrastructure platform is built for enterprises',
+		'category': 'Alerts & Notifications'
 	},
 	{
 		'src':'/img/aws-ecs.svg',
 		'label':'AWS ECS',
-		'overText': 'AWS ECS Infrastructure platform is built for enterprises'
-	},
-	{
-		'src':'/img/teams.svg',
-		'label':'Teams',
-		'overText': 'Teams Infrastructure platform is built for enterprises'
+		'overText': 'AWS ECS Infrastructure platform is built for enterprises',
+		'category': 'Cloud'
 	},
 	{
 		'src':'/img/aws-elastic-beanstalk.svg',
 		'label':'AWS elastic beanstalk',
-		'overText': 'AWS elastic beanstalk Infrastructure platform is built for enterprises'
+		'overText': 'AWS elastic beanstalk Infrastructure platform is built for enterprises',
+		'category': 'Cloud'
 	},
 	{
 		'src':'/img/c-sharp-net.svg',
 		'label':'C#',
-		'overText': 'C# Infrastructure platform is built for enterprises'
+		'overText': 'C# Infrastructure platform is built for enterprises',
+		'category': 'App Tier'
 	},
 	{
 		'src':'/img/custom-metrics-using-statsd.svg',
 		'label':'Custom Metrics',
-		'overText': 'Custom Metrics Infrastructure platform is built for enterprises'
+		'overText': 'Custom Metrics Infrastructure platform is built for enterprises',
+		'category': 'Services'
 	},
 	{
 		'src':'/img/elastic-search.svg',
 		'label':'Elastic Search',
-		'overText': 'Elastic Search Infrastructure platform is built for enterprises'
+		'overText': 'Elastic Search Infrastructure platform is built for enterprises',
+		'category': 'Cloud'
 	},
 	{
 		'src':'/img/generic-etl-workflows.svg',
 		'label':'Generic ETL Workflows',
-		'overText': 'Generic ETL Workflows Infrastructure platform is built for enterprises'
+		'overText': 'Generic ETL Workflows Infrastructure platform is built for enterprises',
+		'category': 'App Tier'
 	},
 	{
 		'src':'/img/generic-webhooks.svg',
 		'label':'Generic Webhooks',
-		'overText': 'Generic Webhooks Infrastructure platform is built for enterprises'
+		'overText': 'Generic Webhooks Infrastructure platform is built for enterprises',
+		'category': 'Services'
 	},
 	{
 		'src':'/img/golang.svg',
 		'label':'Golang',
-		'overText': 'Golang Infrastructure platform is built for enterprises'
+		'overText': 'Golang Infrastructure platform is built for enterprises',
+		'category': 'App Tier'
 	},
 	{
 		'src':'/img/google-authentication.svg',
 		'label':'Google Authentication',
-		'overText': 'Google Authentication Infrastructure platform is built for enterprises'
+		'overText': 'Google Authentication Infrastructure platform is built for enterprises',
+		'category': 'Authentication'
 	},
 	{
 		'src':'/img/ha-proxy.svg',
 		'label':'HA Proxy',
-		'overText': 'HA Proxy Infrastructure platform is built for enterprises'
+		'overText': 'HA Proxy Infrastructure platform is built for enterprises',
+		'category': 'Cloud'
 	},
 	{
 		'src':'/img/iis-server.svg',
 		'label':'IIS Server',
-		'overText': 'IIS Server Infrastructure platform is built for enterprises'
+		'overText': 'IIS Server Infrastructure platform is built for enterprises',
+		'category': 'Cloud'
 	},
 	{
 		'src':'/img/jaeger-for-opentracing.svg',
 		'label':'Jaeger',
-		'overText': 'Jaeger for Opentracing Infrastructure platform is built for enterprises'
+		'overText': 'Jaeger for Opentracing Infrastructure platform is built for enterprises',
+		'category': 'Services'
 	}
 ];
 
-export const  buttonLists = [
+const  buttonLists = [
   {
     'label':'All'
   },
@@ -234,6 +267,9 @@ export const  buttonLists = [
     'label':'App Tier'
   },
   {
+    'label':'Web Tier'
+  },
+  {
     'label':'Databases'
   },
   {
@@ -241,5 +277,106 @@ export const  buttonLists = [
   },
   {
     'label':'Services'
+  },
+  {
+	'label': 'Alerts & Notifications'
   }, 
 ];
+
+const IntegrationsList = () => {
+	const [searchKey, setSearchKey] = useState('');
+	const [selectedCategories, setSelectedCategories] = useState([]);
+	const [categoryAgents, setCategoryAgents] = useState([]);
+	const [filteredAgents, setFilteredAgents] = useState([]);
+
+	const handleSearchKeyChange = (ev) => {
+		const keyword = ev.target.value;
+		if (keyword.length !== 0) {
+			const searchList = categoryAgents.length === 0 ? AGENTS : categoryAgents;
+			const searchAgents = searchList.filter((agent) => agent.label.toLowerCase().includes(keyword.toLowerCase()))
+			setFilteredAgents(searchAgents);
+		} else {
+			setFilteredAgents(categoryAgents);
+		}
+		setSearchKey(keyword);
+	}
+
+	const handleSelectCategory = (evt, categorySelected) => {
+		setSelectedCategories((prevList) => {
+			if (categorySelected !== 'All') {
+				$('.All').removeClass('btnSelected')
+				const categoryPresent = prevList.some((category) => category === categorySelected);
+				let newList = [];
+				if (categoryPresent) {
+					evt.target.classList.remove('btnSelected')
+					newList = prevList.filter((category) => category !== categorySelected)
+				} else {
+					evt.target.classList.add('btnSelected')
+					newList = [...prevList, categorySelected];
+				}
+				const agentsList = newList.length === 0 ? AGENTS : AGENTS.filter((agent) => newList.includes(agent.category))
+				setCategoryAgents(agentsList)
+				if (searchKey.length !== 0) {
+					const searchedAgents = agentsList.filter((agent) => agent.label.toLowerCase().includes(searchKey.toLowerCase()))
+					setFilteredAgents(searchedAgents)
+				} else {
+					setFilteredAgents(agentsList)
+				}
+				return newList;
+			} else {
+				$('.btnSelected').removeClass('btnSelected')
+				evt.target.classList.add('btnSelected')
+				let newList = [];
+				if (prevList.includes(categorySelected)) {
+					newList = [];
+				} else {
+					newList = [categorySelected];
+				}
+				setCategoryAgents(AGENTS);
+				if (searchKey.length !== 0) {
+					const searchedAgents = AGENTS.filter((agent) => agent.label.toLowerCase().includes(searchKey.toLowerCase()))
+					setFilteredAgents(searchedAgents)
+				} else {
+					setFilteredAgents(AGENTS)
+				}
+				return newList;
+			}
+		})
+	}
+
+	const displayAgentsList = () => {
+
+		const finalList = (searchKey.length === 0 && (selectedCategories.length === 0 || selectedCategories.includes('All'))) ? AGENTS : filteredAgents;
+		return finalList.length !== 0 ? finalList.map((items) => (
+			<li key={Math.random()} className="box_size">
+				<label><img src={items.src} /></label>
+				<div>{items.label}</div>
+				<div className="none">{items.overText}</div>
+			</li>
+		)) : (
+			<h4 className="noIntegrations">No integrations found for selected filters</h4>
+		)
+	};
+
+	return (
+		<div>
+			SnappyFlow support a wide range of build in integrations to help you get started quickly.
+			<div className="searchContrainer ">
+				<img src="/img/search.png" alt="Search" />
+				<input type="text" className="form-control" value={searchKey} onChange={handleSearchKeyChange} name="" placeholder="Search for SnappyFlow Integrations" />
+			</div>
+			<dl className="buttonList marTop20">
+			{buttonLists.map((val) => (
+				<dd key={val.label}>
+					<input type="button" value={val.label} onClick={(evt) => handleSelectCategory(evt, val.label)} className={`btn-primarys ${val.label}`} name="" />
+				</dd>
+			))}
+			</dl>
+			<ul className="integration">
+				{displayAgentsList()}
+			</ul>
+		</div>
+	);
+};
+
+export default IntegrationsList;
--- Docusaurus/sf-documentation/docs/Integrations/overview.md
+++ Docusaurus/sf-documentation/docs/Integrations/overview.md
@@ -3,30 +3,6 @@ sidebar_position: 1
 sidebar_label: Overview
 ---
 # SnappyFlow Integrations
-import {AGENTS, buttonLists} from './constants';
+import IntegrationsList from './constants';
 
-SnappyFlow support a wide range of build in integrations to help you get started quickly.
-<div class="searchContrainer ">
-			<img src="/img/search.png" />
-			<input type="text" class="form-control" name="" placeholder="Search for SnappyFlow Integrations" />
-	</div>
-<dl class="buttonList marTop20">
-{
- buttonLists.map((val) => (
-    <dd><input type="button" value={val.label} class="btn-primarys" name=""  /></dd>
- ))
-}
-</dl>
-<ul class="integration">
-{
- AGENTS.map((items) => (
-<li class="box_size">
- <label>
-            <img src={items.src} />
-    </label>
-    <div>{items.label}</div>
-    <div class="none">{items.overText}</div>
-</li>
- ))
- }
-</ul>
+<IntegrationsList />
