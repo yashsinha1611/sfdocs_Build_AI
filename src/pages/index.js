e955fd4b28a6bff4d481906e905ecad19f11ce39
Anbukumar C <anbukumar.c@maplelabs.com>
2022-07-28 12:01:39 +0530

    Intercom chat implemented.

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,18 +4,10 @@ import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
-import HomepageFeatures from '../components/HomepageFeatures';
-
+import HomepageFeatures from '../components/HomepageFeatures'; 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
-  // useEffect(() => {
-  //   if (typeof window !== undefined) {
-  //     if (window.location.host.includes('localhost')) {
-  //       document.querySelector('.navbar__brand').href = StageHomePageUrl
-  //       // siteConfig.themeConfig.navbar.logo.href = StageHomePageUrl
-  //     }
-  //   }
-  // });
+   
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className={styles.banner_container}>
@@ -33,14 +25,25 @@ function HomepageHeader() {
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
+  useEffect(() => { 
+        // Intercom chat // 
+        window.intercomSettings = {
+          api_base: "https://api-iam.intercom.io",
+          app_id: "q7tpwd9w"
+        }; 
+        (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/q7tpwd9w';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+ 
+    },[]);
   return (
     <Layout
       title={`${siteConfig.title}`}
       description="<head />">
       <HomepageHeader />
       <main >
-        <HomepageFeatures />
-      </main>
+        <HomepageFeatures /> 
+      </main> 
+          
+      
     </Layout>
   );
 }
